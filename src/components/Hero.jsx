2f739f99e1550f9b0d93fb049d113946e95c2796
Nitto Dev <nittodevelopers@gmail.com>
2024-07-14 05:04:40 +0600

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         {/* <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" /> */}
-        <img src={itsolutions} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <img src={itsolutions} alt="billing" className="w-[100%] h-[85%] relative z-[5]" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
